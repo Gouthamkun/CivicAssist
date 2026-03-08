76a73a9923cfd39ec01fe72ef42b1f3afaaaae22
Gouthamkun <5604gouthamkundeti@gmail.com>
2026-03-08 23:07:46 +0530

    Updated with frontend

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from backend.services.assistant import civic_assist
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins for dev
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class QueryRequest(BaseModel):
     question: str
-
-
-@app.get("/")
-def home():
-    return {"message": "CivicAssist AI running"}
-
 
 @app.post("/ask")
 def ask(request: QueryRequest):
     response = civic_assist(request.question)
     return {"response": response}
+
+app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
